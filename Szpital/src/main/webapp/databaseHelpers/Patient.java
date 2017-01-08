cd8678bd45cf03533ed2f485544514582bb65f9d
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2017-01-08 17:08:47 +0100

    Adding function to adding new patients

--- Szpital/src/main/webapp/databaseHelpers/Patient.java
+++ Szpital/src/main/webapp/databaseHelpers/Patient.java
@@ -2,6 +2,32 @@ package main.webapp.databaseHelpers;
 
 public class Patient {
 
+	public static boolean addNewPatient(String name, String surname, String city, String address, String birthDay,
+			int hospitalWard_ID) {
+
+		boolean result;
+		String SQL = "INSERT INTO Pacjent VALUES ('" + name + "', '" + surname + "', '" + city + "', '" + address
+				+ "', '" + birthDay + "', " + hospitalWard_ID + ")";
+
+		result = Database.insertUpdateIntoDatabase(SQL);
+
+		if (result) {
+
+			SQL = "SELECT Ilosc_pac FROM Oddzial WHERE Oddzial_ID = " + hospitalWard_ID;
+			int amoutOfPatients = Integer.parseInt(Database.selectFromDatabase(SQL, "Ilosc_pac")) + 1;
+
+			SQL = "UPDATE Oddzial SET Ilosc_pac = " + amoutOfPatients + "WHERE Oddzial_ID = " + hospitalWard_ID;
+			result = Database.insertUpdateIntoDatabase(SQL);
+		}
+		return result;
+	}
+	
+	
+	public static boolean removePatient(int patientID){
+		
+		return false;
+	}
+
 	public static String checkStatus(String surname) {
 
 		String SQL = "SELECT P.*, Od.Nazwa, L.Nazwa, PL.Il_dawek FROM Pacjent P JOIN "
@@ -13,16 +39,16 @@ public class Patient {
 		return result;
 
 	}
-	
-	
-	public static String checkDrugs(String name, String surname){
-		
+
+	public static String checkDrugs(String name, String surname) {
+
 		String SQL = "SELECT P.Pacjent_ID, P.Imie, P.Nazwisko, P.Data_ur, L.Nazwa, PL.Il_dawek "
 				+ "FROM Pacjent P JOIN Pacjent_Leki_Junction PL on PL.Pacjent_ID = P.Pacjent_ID "
-				+ "JOIN Leki L on PL.Leki_ID = L.Leki_ID WHERE P.Imie LIKE '"+name+"' AND P.Nazwisko LIKE '"+surname+"'";
+				+ "JOIN Leki L on PL.Leki_ID = L.Leki_ID WHERE P.Imie LIKE '" + name + "' AND P.Nazwisko LIKE '"
+				+ surname + "'";
 		String result = Database.selectFromDatabase(SQL);
 
 		return result;
 	}
-	
+
 }
--- Szpital/src/main/webapp/java/TestClass.java
+++ Szpital/src/main/webapp/java/TestClass.java
@@ -1,5 +1,6 @@
 package main.webapp.java;
 
+import main.webapp.databaseHelpers.Database;
 import main.webapp.databaseHelpers.Drug;
 import main.webapp.databaseHelpers.LoginValidate;
 import main.webapp.databaseHelpers.Patient;
@@ -21,10 +22,10 @@ public class TestClass {
 //		System.out.println(Drug.removeDrugFromPatient(1, 1, 3));
 		
 //		System.out.println(Drug.addAmountOfDrug(1, 2));
+//		System.out.println(Patient.addNewPatient("Bartek2", "Foszmanowicz", "Łódź", "28PSK 6/10", "1993-12-13", 4));
+		System.out.println(Patient.checkStatus("Foszmanowicz"));
 		
-		System.out.println(Patient.checkStatus("Paździoch"));
-		
-		System.out.println(PatientsInHospitalWard.checkPatientsInHospitalWard("Oddział kardiologii"));
+		System.out.println(PatientsInHospitalWard.checkPatientsInHospitalWard("Oddział psychiatryczny"));
 		
 		
 		
@@ -34,10 +35,12 @@ public class TestClass {
 		System.out.println("Ilosc leku");
 		System.out.println(Drug.checkAmountOfDrug(1));
 		
-		
+		System.out.println(Database.selectFromDatabase("SELECT * FROM Pacjent"));
 		if(3>=3){
 			System.out.println("yes");
 		}
+		
+//		System.out.println(Patient.addNewPatient("Bartek", "Foszmanowicz", "Łódź", "28PSK 6/10", "1993-12-13", 4));
 //		if(AddDrug.checkPatientHaveAlreadyThisDrug(1,2)){
 //			System.out.println("Yes");
 //		}
